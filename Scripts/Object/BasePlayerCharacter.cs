2e35f3e9c8d678b6e09355a8ceb547f69e8e184c
Host <Host>
2022-11-21 15:26:42 +0900

    - WSAD 키 입력/키 뗏을 씨 함수 구현 추가

--- Scripts/Object/BasePlayerCharacter.cs
+++ Scripts/Object/BasePlayerCharacter.cs
@@ -1,6 +1,27 @@
 
 // 플레이어 기본 뼈대.
 
+/*
+    - 구현 목록 -
+
+    1. WSAD 이동 구현.
+    2. 마우스를 통해 좌, 우 회전 구현.
+    3. m_Speed 변수를 통해 이동속도 조절 가능.
+
+    Move함수와 ThreeView함수는 상속받으면 Update문에 선언해줄 것.
+
+    ex)
+    public class Player : BasePlayerCharacter
+    {
+        private void Update()
+        {
+            ThreeView();
+            Move();
+        }
+    }
+ 
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,25 +43,49 @@ public class BasePlayerCharacter : BaseCharacter
 
     // 캐릭터 이동 WSAD.
     public virtual void Move()
-    {        
+    {
+        // 키를 꾹 입력 했을 시.
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * m_Speed * Time.deltaTime);
+            W_Key();
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * m_Speed * Time.deltaTime);
+            S_Key();
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * m_Speed * Time.deltaTime);
+            A_Key();
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * m_Speed * Time.deltaTime);
+            D_Key();
+        }
+
+
+
+        // 키를 떼었을 경우.
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            W_KeyUp();
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            S_KeyUp();
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            A_KeyUp();
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            D_KeyUp();
         }
     }
 
@@ -55,5 +100,49 @@ public class BasePlayerCharacter : BaseCharacter
         qt.eulerAngles = new Vector3(qt.eulerAngles.x, qt.eulerAngles.y + posX, qt.eulerAngles.z);
 
         transform.rotation = qt;
+    }
+
+    // 앞 움직임.
+    public virtual void W_Key()
+    {
+        transform.Translate(Vector3.forward * m_Speed * Time.deltaTime);
+    }
+
+    // 앞 움직임 키 뗐을 시.
+    public virtual void W_KeyUp()
+    {
+    }
+
+    // 뒤 움직임.
+    public virtual void S_Key()
+    {
+        transform.Translate(Vector3.back * m_Speed * Time.deltaTime);
+    }
+
+    // 뒤 움직임 키 뗏을 시.
+    public virtual void S_KeyUp()
+    {
+    }
+
+    // 왼쪽 움직임.
+    public virtual void A_Key()
+    {
+        transform.Translate(Vector3.left * m_Speed * Time.deltaTime);
+    }
+
+    // 왼쪽 움직임 키 뗏을 시.
+    public virtual void A_KeyUp()
+    {        
+    }
+
+    // 오른쪽 움직임.
+    public virtual void D_Key()
+    {
+        transform.Translate(Vector3.right * m_Speed * Time.deltaTime);
+    }
+
+    // 오른쪽 움직임 키 뗏을 시.
+    public virtual void D_KeyUp()
+    {
     }
 }
