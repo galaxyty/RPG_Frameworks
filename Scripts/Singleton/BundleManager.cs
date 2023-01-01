838a2c83613da685c9f25456b52ea98dbdd459d3
Host <Host>
2023-01-01 17:24:27 +0900

    - json 에셋 불러오는 방식 동기로 변경

--- Scripts/Singleton/BundleManager.cs
+++ Scripts/Singleton/BundleManager.cs
@@ -8,41 +8,19 @@ using BaseRPG_V1;
 
 public class BundleManager : BaseSingleton<BundleManager>
 {
-    // 번들 다운로드.
-    public void DownloadBundleAsync()
+    // scripts 레이블에서 해당 key 번들 로드 (동기).
+    public TextAsset LoadToScripts(string key)
     {
-        // scripts 테이블 초기화.
-        Addressables.DownloadDependenciesAsync(Constants.kLABLE.scripts.ToString()).Completed += (AsyncOperationHandle handle) =>
-        {
-            // 아이템 테이블 매니저 초기화.
-            LoadToScripts(Constants.kBUNDLE.ITEM.ToString());
+        TextAsset obj = Addressables.LoadAssetAsync<TextAsset>(key).WaitForCompletion();
 
-            Addressables.Release(handle);
-        };
-
-        // Characters 테이블 초기화.
-        Addressables.DownloadDependenciesAsync(Constants.kLABLE.characters.ToString()).Completed += (AsyncOperationHandle handle) =>
-        {
-            Addressables.Release(handle);
-        };
-    }
-
-    // scripts 레이블에서 해당 key 번들 로드.
-    public void LoadToScripts(string key)
-    {
-        // 어드레서블 에셋에서 로드 후 테이블매니저에 캐싱.
-        Addressables.LoadAssetAsync<TextAsset>(key).Completed += (AsyncOperationHandle<TextAsset> obj) =>
-        {
-            TextAsset text = obj.Result;
-
-            TableManager.Instance.Add(key, text);            
-        };
+        return obj;
     }
 
     // key 오브젝트 씬에 생성 (동기).
     public void Instantiate(string key, Action<GameObject> callback)
     {        
         var obj = Addressables.InstantiateAsync(key).WaitForCompletion();
+        
         callback(obj);
     }
 }
--- Scripts/Singleton/TableManager.cs
+++ Scripts/Singleton/TableManager.cs
@@ -5,21 +5,6 @@ using BaseRPG_V1;
 
 public class TableManager : BaseSingleton<TableManager>
 {
-    // 캐싱 테이블.
-    private Dictionary<string, TextAsset> m_Dic = new Dictionary<string, TextAsset>();
-
-    // 캐싱 테이블 생성.
-    public void Add(string key, TextAsset asset)
-    {
-        m_Dic.Add(key, asset);
-    }
-
-    // 캐싱 테이블 삭제.
-    public void Clear()
-    {
-        m_Dic.Clear();
-    }
-
     // 아이템 테이블 반환.
     public ItemData GetItemData()
     {
@@ -33,12 +18,9 @@ public class TableManager : BaseSingleton<TableManager>
 
     // Key 테이블 반환.
     private T GetTable<T>(string key)
-    {
-        if (m_Dic.ContainsKey(key) == false)
-        {
-            return default(T);
-        }
+    {    
+        TextAsset text = BundleManager.Instance.LoadToScripts(key);
 
-        return JsonUtility.FromJson<T>(m_Dic[key].ToString());
+        return JsonUtility.FromJson<T>(text.ToString());
     }
 }
