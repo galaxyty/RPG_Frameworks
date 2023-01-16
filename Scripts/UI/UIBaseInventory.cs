a53e533956965edab0f12b4ea03ffb826338df1b
Host <Host>
2023-01-16 16:33:10 +0900

    - Awake 삭제

--- Scripts/UI/UIBaseInventory.cs
+++ Scripts/UI/UIBaseInventory.cs
@@ -6,17 +6,14 @@ using BaseRPG_V1;
 public class UIBaseInventory : BaseInventory
 {
     // 인벤토리 슬롯.
-    private List<UIBaseInventorySlot> m_ListOfSlot = new List<UIBaseInventorySlot>();
-
-    private void Awake() 
-    {
-        // 인벤토리 슬롯 풀 생성.
-        PoolManager.Instance.Create<UIBaseInventorySlot>(Constants.kBUNDLE.InventorySlot.ToString(), m_Count);    
-    }
+    private List<UIBaseInventorySlot> m_ListOfSlot = new List<UIBaseInventorySlot>();    
 
     public override void Initialization()
     {
         base.Initialization();
+
+        // 인벤토리 슬롯 풀 생성.
+        PoolManager.Instance.Create<UIBaseInventorySlot>(Constants.kBUNDLE.InventorySlot.ToString(), m_Count);
 
         // 인벤토리 슬롯 생성.
         for (int i = 0; i < m_Count; i++)
