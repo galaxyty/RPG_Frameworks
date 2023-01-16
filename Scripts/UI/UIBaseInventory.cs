6d83cfc2c97356ecb53ede56539f9650c5aea9af
Host <Host>
2023-01-16 17:46:29 +0900

    - Awake 복구

--- Scripts/UI/UIBaseInventory.cs
+++ Scripts/UI/UIBaseInventory.cs
@@ -8,12 +8,15 @@ public class UIBaseInventory : BaseInventory
     // 인벤토리 슬롯.
     private List<UIBaseInventorySlot> m_ListOfSlot = new List<UIBaseInventorySlot>();    
 
+    private void Awake() 
+    {
+        // 인벤토리 슬롯 풀 생성.
+        PoolManager.Instance.Create<UIBaseInventorySlot>(Constants.kBUNDLE.InventorySlot.ToString(), m_Count);    
+    }
+
     public override void Initialization()
     {
-        base.Initialization();
-
-        // 인벤토리 슬롯 풀 생성.
-        PoolManager.Instance.Create<UIBaseInventorySlot>(Constants.kBUNDLE.InventorySlot.ToString(), m_Count);
+        base.Initialization();        
 
         // 인벤토리 슬롯 생성.
         for (int i = 0; i < m_Count; i++)
