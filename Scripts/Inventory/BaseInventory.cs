3ac6b55f78b47e932916280fccbdfeaf3535f208
Host <Host>
2023-01-18 22:19:23 +0900

    - 인벤토리 슬롯 Header 추가 - 새 프레임워크 갱신

--- Scripts/Inventory/BaseInventory.cs
+++ Scripts/Inventory/BaseInventory.cs
@@ -7,10 +7,12 @@ namespace BaseRPG_V1
     public class BaseInventory : BaseObject
     {
         // 슬롯 갯수.
+        [Header("슬롯 생성 갯수")]
         [SerializeField]
         protected int m_Count;
 
         // 그리드 영역.
+        [Header("슬롯 생성 할 영역")]
         [SerializeField]
         protected GameObject m_Grid;
 
