1e2127ff04b8ef6bff8318ab48b6a92de6c13917
Host <Host>
2023-04-13 18:30:02 +0900

    - BundleManager 새 using문 추가

--- Scripts/Singleton/BundleManager.cs
+++ Scripts/Singleton/BundleManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using BaseRPG_V1;
 
 public class BundleManager : BaseSingleton<BundleManager>
